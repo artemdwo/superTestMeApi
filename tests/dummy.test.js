4f406f167029a75ae0ed23d9f6a25a265b60d891
Artem Dvornichenko <artemdwo@gmail.com>
2018-08-21 16:49:53 +0100

    [UPDATE] Method POST test
    /create endpoint returns created record with an ID assigned to it

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,9 +1,11 @@
 const request = require('supertest')('http://dummy.restapiexample.com/api/v1')
 const chai = require('chai')
 const expect = chai.expect
+const should = chai.should()
 
 chai.use(require('chai-arrays'))
 chai.use(require('chai-json'))
+chai.use(require('chai-like'))
 
 describe('Method GET', function() {
   context('over /employees endpoint', function() {
@@ -44,6 +46,32 @@ describe('Method GET', function() {
       .get('/employee/9999999')
       .expect(200)
       .expect('false',done)
+    })
+  })
+})
+
+describe('Method POST', () => {
+  context('over /create endpoint', () => {
+    
+    const employee = {
+      name: 'Jonny Doe',
+      salary: '0.11',
+      age: '199'
+    }
+
+    it('returns created record with it\'s id prop', (done) => {
+      request
+      .post('/create')
+      .send(employee)
+      .expect(200)
+      .end((err, result) => {
+        if (err) done(err)
+
+        let response = JSON.parse(result.text)
+        response.should.like(employee)
+        expect(response).to.have.property('id')
+        done()
+      })
     })
   })
 })
