31fbc2cb265dc3a46713c753393b0bb24c8f9e42
Artem Dvornichenko <artemdwo@gmail.com>
2018-08-21 16:42:45 +0100

    [UPDATE] Method GET tests
    /employees endpoint returns non-empty array

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -2,12 +2,27 @@ const request = require('supertest')('http://dummy.restapiexample.com/api/v1')
 const chai = require('chai')
 const expect = chai.expect
 
+chai.use(require('chai-arrays'))
+
 describe('Method GET', function() {
   context('over /employees endpoint', function() {
     it('returns 200 when called', function(done) {
       request
         .get('/employees')
         .expect(200, done)
+    })
+
+    it('returns non-empty array of data', function(done) {
+      request
+        .get('/employees')
+        .end(function (err, result) {
+          if (err) done(err)
+
+          let data = result.text
+          expect(data).to.be.array
+          expect(data).is.not.empty
+          done()
+        })
     })
   })
 })
